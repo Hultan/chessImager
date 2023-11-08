bb286d2e074dc28351fc19917a8b1776852f561d
per <per@softteam.se>
2023-11-08 18:33:10 +0100

    Tests - simple, medium

---
+++ test/examples_test.go
@@ -0,0 +1,66 @@
+package test
+
+import (
+	"image"
+	"image/png"
+	"os"
+	"testing"
+
+	"github.com/Hultan/chessImager"
+)
+
+func TestSimpleExample(t *testing.T) {
+	filename := "simple.png"
+
+	const fen = "b2r3r/k3Rp1p/p2q1np1/Np1P4/3p1Q2/P4PPB/1PP4P/1K6 b - - 1 25"
+	img, err := chessImager.NewImager().Render(fen)
+	if err != nil {
+		t.Error(err)
+	}
+
+	createAndCompare(t, filename, img)
+}
+
+func TestMediumExample(t *testing.T) {
+	filename := "medium.png"
+
+	// Create a new imager using embedded default.json settings
+	imager := chessImager.NewImager()
+
+	// Create a new context
+	ctx := imager.NewContext()
+
+	// Highlight square e7
+	// Annotate square e7 with "!!"
+	// Show move e1-e7
+	ctx.AddHighlight("e7").AddAnnotation("e7", "!!").AddMove("e1", "e7")
+
+	// Render the image
+	const fen = "b2r3r/k3Rp1p/p2q1np1/Np1P4/3p1Q2/P4PPB/1PP4P/1K6 b - - 1 25"
+	img, err := imager.RenderEx(fen, ctx)
+	if err != nil {
+		t.Errorf("failed to render : %v", err)
+	}
+
+	createAndCompare(t, filename, img)
+}
+
+func createAndCompare(t *testing.T, filename string, img image.Image) {
+	file, err := os.Create(filename)
+	if err != nil {
+		t.Errorf("failed to create : %v", err)
+	}
+	defer file.Close()
+	err = png.Encode(file, img)
+	if err != nil {
+		t.Errorf("failed to encode : %v", err)
+	}
+
+	ok, err := compareFiles(filename, "valid/"+filename)
+	if err != nil {
+		t.Errorf("error during compare : %v", err)
+	}
+	if !ok {
+		t.Errorf("failed to compare, images differ!")
+	}
+}
---
+++ test/misc.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"image"
+	"image/png"
+	"os"
+)
+
+func compareFiles(f1, f2 string) (bool, error) {
+	i1, err := loadImage(f1)
+	if err != nil {
+		return false, err
+	}
+	i2, err := loadImage(f2)
+	if err != nil {
+		return false, err
+	}
+
+	if !i1.Bounds().Eq(i2.Bounds()) {
+		return false, nil
+	}
+
+	for y := 0; y < i1.Bounds().Size().Y; y++ {
+		for x := 0; x < i1.Bounds().Size().X; x++ {
+			if i1.At(x, y) != i2.At(x, y) {
+				return false, nil
+			}
+		}
+	}
+
+	return true, nil
+}
+
+func loadImage(f string) (image.Image, error) {
+	file, err := os.Open(f)
+	if err != nil {
+		return nil, err
+	}
+	img, err := png.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
