f5c8de00af7a1d23e9a83d27b0c70422a966cbac
per <per@softteam.se>
2023-11-09 15:35:55 +0100

    test bugfix and removed dead code

--- test/border_test.go
+++ test/border_test.go
@@ -13,11 +13,13 @@ func TestBorder(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to load JSON file: %v", err)
 	}
-	ctx := imager.NewContext()
 
 	// Render the image
 	const fen = "b2r3r/k3Rp1p/p2q1np1/Np1P4/3p1Q2/P4PPB/1PP4P/1K6 b - - 1 25"
-	img, _ := imager.RenderEx(fen, ctx)
+	img, err := imager.Render(fen)
+	if err != nil {
+		t.Fatalf("Failed to render chess board: %v", err)
+	}
 
 	compareImages(t, filename, &img)
 }
