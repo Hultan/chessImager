4830a072ad546505bb2eec46015493da3ecf9d32
per <per@softteam.se>
2023-11-12 11:45:31 +0100

    Refactor

--- rendererMoves.go
+++ rendererMoves.go
@@ -27,28 +27,21 @@ func (r *rendererMoves) draw(c *gg.Context) error {
 func (r *rendererMoves) renderMove(c *gg.Context, move Move) error {
 	style := r.getStyle(move)
 
-	from, err := newAlg(move.From)
+	fromX, fromY, toX, toY, err := r.getFromAndTo(move)
 	if err != nil {
 		return err
 	}
-	fromX, fromY := from.coords()
-
-	to, err := newAlg(move.To)
-	if err != nil {
-		return err
-	}
-	toX, toY := to.coords()
 
 	dx, dy := toX-fromX, toY-fromY
-	x, y := fromX, fromY
-
 	if dx == 0 && dy == 0 {
 		return nil // Ignore no move
 	}
+
 	c.SetRGBA(style.Color.toRGBA())
 
 	switch style.Type {
 	case MoveTypeDots:
+		x, y := fromX, fromY
 		if dx == 0 || dy == 0 || abs(dx) == abs(dy) {
 			// Render pawn, rook, bishop, king and queen moves (ie straight moves)
 			d := max(abs(dx), abs(dy))
@@ -65,27 +58,107 @@ func (r *rendererMoves) renderMove(c *gg.Context, move Move) error {
 				r.renderDottedLine(c, &x, &y, 0, sgn(dy), abs(dy), style)
 			}
 		}
-		// TODO : How to handle castling?
 	case MoveTypeArrow:
-		var rect Rectangle
 		fx, fy := getSquareBox(fromX, fromY).center()
-		rect, err = r.getNextToLast(move)
+		rect, err := r.getNextToLast(move)
 		if err != nil {
 			return err
 		}
 		tx, ty := rect.center()
-		box := rect.shrink(style.Factor)
+		styleBox := rect.shrink(style.Factor)
 
-		c.SetLineWidth(box.Width)
-		c.DrawLine(fx, fy, tx, ty)
-		c.Stroke()
-
-		r.renderArrowHead(c, rect, rect.Width, box.Width, dx, dy)
+		r.renderArrowLine(c, styleBox.Width, fx, fy, tx, ty)
+		r.renderArrowHead(c, styleBox.Width, rect, dx, dy)
 	default:
 		return errors.New("illegal move type")
 	}
 
 	return nil
+}
+
+// Render arrow head for all types of moves
+func (r *rendererMoves) renderArrowLine(c *gg.Context, width, fx, fy, tx, ty float64) {
+	c.SetLineWidth(width)
+	c.DrawLine(fx, fy, tx, ty)
+	c.Stroke()
+}
+
+// Render arrow head for all types of moves
+func (r *rendererMoves) renderArrowHead(c *gg.Context, width float64, rect Rectangle, dx, dy int) {
+	switch {
+	case dx == 0 && dy > 0:
+		// Vertical move up (queen, rook, king, pawn)
+		r.renderArrowHeadInDirection(c, rect, width, directionNorth)
+		return
+	case dx == 0 && dy < 0:
+		// Vertical move down (queen, rook, king)
+		r.renderArrowHeadInDirection(c, rect, width, directionSouth)
+		return
+	case dx < 0 && dy == 0:
+		// Horizontal move left (queen, rook, king)
+		r.renderArrowHeadInDirection(c, rect, width, directionWest)
+		return
+	case dx > 0 && dy == 0:
+		// Horizontal move right (queen, rook, king)
+		r.renderArrowHeadInDirection(c, rect, width, directionEast)
+		return
+	case abs(dx) == abs(dy):
+		// Bishop move, king (diagonal), queen (diagonal) or pawn (when capturing)
+		switch {
+		case dx > 0 && dy > 0:
+			// NE
+			r.renderArrowHeadInDirection(c, rect, width, directionNorthEast)
+		case dx > 0 && dy < 0:
+			// SE
+			r.renderArrowHeadInDirection(c, rect, width, directionSouthEast)
+		case dx < 0 && dy > 0:
+			// NW
+			r.renderArrowHeadInDirection(c, rect, width, directionNorthWest)
+		case dx < 0 && dy < 0:
+			// SW
+			r.renderArrowHeadInDirection(c, rect, width, directionSouthWest)
+		}
+		return
+	case false:
+		// Castling, how to handle
+
+	// Knight moves
+	case (dx == 2 && dy == 1) || (dx == -2 && dy == 1):
+		r.renderArrowHeadInDirection(c, rect, width, directionNorth)
+	case (dx == 1 && dy == 2) || (dx == 1 && dy == -2):
+		r.renderArrowHeadInDirection(c, rect, width, directionEast)
+	case (dx == 2 && dy == -1) || (dx == -2 && dy == -1):
+		r.renderArrowHeadInDirection(c, rect, width, directionSouth)
+	case (dx == -1 && dy == 2) || (dx == -1 && dy == -2):
+		r.renderArrowHeadInDirection(c, rect, width, directionWest)
+
+	default:
+		// Illegal moves
+		return
+	}
+}
+
+func (r *rendererMoves) renderArrowHeadInDirection(c *gg.Context, rect Rectangle, width float64, dir direction) {
+	cx, cy := rect.center()
+
+	// Rotate to draw in correct angle
+	c.RotateAbout(gg.Radians(float64(dir)), cx, cy)
+
+	// Draw line
+	c.SetLineWidth(abs(width))
+	c.DrawLine(cx, cy, cx, cy-rect.Width/2+width)
+	c.Stroke()
+
+	// Draw arrow head (triangle part)
+	c.SetLineWidth(1)
+	c.MoveTo(cx+width, cy-rect.Width/2+width)
+	c.LineTo(cx-width, cy-rect.Width/2+width)
+	c.LineTo(cx, cy-rect.Width/2)
+	c.ClosePath()
+	c.Fill()
+
+	// Rotate back
+	c.RotateAbout(gg.Radians(float64(-dir)), cx, cy)
 }
 
 func (r *rendererMoves) renderDottedLine(c *gg.Context, x, y *int, dx, dy, moves int, style *MoveStyle) {
@@ -111,98 +184,13 @@ func (r *rendererMoves) getStyle(move Move) *MoveStyle {
 	}
 }
 
-// Render arrow head for all types of moves
-func (r *rendererMoves) renderArrowHead(c *gg.Context, rect Rectangle, square, width float64, dx, dy int) {
-	switch {
-	case dx == 0 && dy > 0:
-		// Vertical move up (queen, rook, king, pawn)
-		r.renderArrowHeadInDirection(c, rect, square, width, directionNorth)
-		return
-	case dx == 0 && dy < 0:
-		// Vertical move down (queen, rook, king)
-		r.renderArrowHeadInDirection(c, rect, square, width, directionSouth)
-		return
-	case dx < 0 && dy == 0:
-		// Horizontal move left (queen, rook, king)
-		r.renderArrowHeadInDirection(c, rect, square, width, directionWest)
-		return
-	case dx > 0 && dy == 0:
-		// Horizontal move right (queen, rook, king)
-		r.renderArrowHeadInDirection(c, rect, square, width, directionEast)
-		return
-	case abs(dx) == abs(dy):
-		// Bishop move, king (diagonal), queen (diagonal) or pawn (when capturing)
-		switch {
-		case dx > 0 && dy > 0:
-			// NE
-			r.renderArrowHeadInDirection(c, rect, square, width, directionNorthEast)
-		case dx > 0 && dy < 0:
-			// SE
-			r.renderArrowHeadInDirection(c, rect, square, width, directionSouthEast)
-		case dx < 0 && dy > 0:
-			// NW
-			r.renderArrowHeadInDirection(c, rect, square, width, directionNorthWest)
-		case dx < 0 && dy < 0:
-			// SW
-			r.renderArrowHeadInDirection(c, rect, square, width, directionSouthWest)
-		}
-		return
-	case false:
-		// Castling, how to handle
-
-	// Knight moves
-	case (dx == 2 && dy == 1) || (dx == -2 && dy == 1):
-		r.renderArrowHeadInDirection(c, rect, square, width, directionNorth)
-	case (dx == 1 && dy == 2) || (dx == 1 && dy == -2):
-		r.renderArrowHeadInDirection(c, rect, square, width, directionEast)
-	case (dx == 2 && dy == -1) || (dx == -2 && dy == -1):
-		r.renderArrowHeadInDirection(c, rect, square, width, directionSouth)
-	case (dx == -1 && dy == 2) || (dx == -1 && dy == -2):
-		r.renderArrowHeadInDirection(c, rect, square, width, directionWest)
-
-	default:
-		// Illegal moves
-		return
-	}
-}
-
-func (r *rendererMoves) renderArrowHeadInDirection(c *gg.Context, rect Rectangle, square, width float64, dir direction) {
-	cx, cy := rect.center()
-
-	// Rotate to draw in correct angle
-	c.RotateAbout(gg.Radians(float64(dir)), cx, cy)
-
-	// Draw line
-	c.SetLineWidth(abs(width))
-	c.DrawLine(cx, cy, cx, cy-square/2+width)
-	c.Stroke()
-
-	// Draw arrow head (triangle part)
-	c.SetLineWidth(1)
-	c.MoveTo(cx+width, cy-square/2+width)
-	c.LineTo(cx-width, cy-square/2+width)
-	c.LineTo(cx, cy-square/2)
-	c.ClosePath()
-	c.Fill()
-
-	// Rotate back
-	c.RotateAbout(gg.Radians(float64(-dir)), cx, cy)
-}
-
 // Returns next to last square in a move.
 // For example if rook moves from d1 to d7, the d6 square will be returned
 func (r *rendererMoves) getNextToLast(move Move) (Rectangle, error) {
-	from, err := newAlg(move.From)
+	fromX, fromY, toX, toY, err := r.getFromAndTo(move)
 	if err != nil {
 		return Rectangle{}, err
 	}
-	fromX, fromY := from.coords()
-
-	to, err := newAlg(move.To)
-	if err != nil {
-		return Rectangle{}, err
-	}
-	toX, toY := to.coords()
 
 	dx, dy := toX-fromX, toY-fromY
 
@@ -220,4 +208,20 @@ func (r *rendererMoves) getNextToLast(move Move) (Rectangle, error) {
 	}
 
 	return Rectangle{}, errors.New("invalid move")
+}
+
+func (r *rendererMoves) getFromAndTo(move Move) (int, int, int, int, error) {
+	from, err := newAlg(move.From)
+	if err != nil {
+		return 0, 0, 0, 0, err
+	}
+	fromX, fromY := from.coords()
+
+	to, err := newAlg(move.To)
+	if err != nil {
+		return 0, 0, 0, 0, err
+	}
+	toX, toY := to.coords()
+
+	return fromX, fromY, toX, toY, nil
 }
