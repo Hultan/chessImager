b97b49cd964c1beb47d966de70dcad55fb1635df
per <per@softteam.se>
2023-11-15 07:37:09 +0100

    Fixed medium example

--- examples/medium/medium.go
+++ examples/medium/medium.go
@@ -17,7 +17,7 @@ func main() {
 	// Highlight square e7
 	// Annotate square e7 with "!!"
 	// Show move e1-e7
-	ctx.AddHighlight("e7").AddAnnotation("e7", "!!").AddMove("f6", "h5")
+	ctx.AddHighlight("e7").AddAnnotation("e7", "!!").AddMove("e1", "e7")
 
 	// Render the image
 	const fen = "b2r3r/k3Rp1p/p2q1np1/Np1P4/3p1Q2/P4PPB/1PP4P/1K6 b - - 1 25"
