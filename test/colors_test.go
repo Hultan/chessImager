e5e3588c8f86ca9abb8069bd52df1d7fc0da6a6c
per <per@softteam.se>
2023-11-21 13:00:37 +0100

    Test for colors

---
+++ test/colors_test.go
@@ -0,0 +1,55 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/Hultan/chessImager"
+)
+
+func TestColors(t *testing.T) {
+	// Create a new context
+	ctx := chessImager.NewImager().NewContext()
+
+	// Test color #RRGGBBAA
+	s, err := ctx.NewMoveStyle(chessImager.MoveTypeDots, "#9D6B5EFF", 0.2)
+	if err != nil {
+		t.Fatalf("failed to create style with color #RRGGBBAA: %v", err)
+	}
+	if !validateColor(s) {
+		t.Fatalf("failed to validate style with color #RRGGBBAA: %v", err)
+	}
+
+	// Test color #RRGGBB
+	s, err = ctx.NewMoveStyle(chessImager.MoveTypeDots, "#9D6B5E", 0.2)
+	if err != nil {
+		t.Fatalf("failed to create style with color #RRGGBB: %v", err)
+	}
+	if !validateColor(s) {
+		t.Fatalf("failed to validate style with color #RRGGBB: %v", err)
+	}
+
+	// Test color RRGGBBAA
+	s, err = ctx.NewMoveStyle(chessImager.MoveTypeDots, "9D6B5EFF", 0.2)
+	if err != nil {
+		t.Fatalf("failed to create style with color RRGGBBAA: %v", err)
+	}
+	if !validateColor(s) {
+		t.Fatalf("failed to validate style with color RRGGBBAA: %v", err)
+	}
+
+	// Test color RRGGBB
+	s, err = ctx.NewMoveStyle(chessImager.MoveTypeDots, "9D6B5E", 0.2)
+	if err != nil {
+		t.Fatalf("failed to create style with color RRGGBB: %v", err)
+	}
+	if !validateColor(s) {
+		t.Fatalf("failed to validate style with color RRGGBB: %v", err)
+	}
+}
+
+func validateColor(s *chessImager.MoveStyle) bool {
+	if s.Color.R == 157 && s.Color.G == 107 && s.Color.B == 94 && s.Color.A == 255 {
+		return true
+	}
+	return false
+}
