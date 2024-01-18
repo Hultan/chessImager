8c99e86c1a10638b67681edbf1d90d8e0565cc2e
per <per@softteam.se>
2024-01-18 12:18:54 +0100

    Refactoring (misc)

--- chessImager.go
+++ chessImager.go
@@ -3,19 +3,26 @@ package chessImager
 import (
 	_ "embed"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"image"
+	"image/color"
 	"io"
 	"os"
 	"strings"
 
 	"github.com/fogleman/gg"
 	"github.com/golang/freetype/truetype"
+	"golang.org/x/exp/constraints"
 	"golang.org/x/image/font/gofont/goregular"
 )
 
 //go:embed config/default.json
 var defaultSettings string
+
+type renderer interface {
+	draw(*gg.Context, *ImageContext) error
+}
 
 // Imager is the main struct that is used to create chess board images
 type Imager struct {
@@ -256,4 +263,51 @@ func decodeSettings(r io.Reader) (*Settings, error) {
 	}
 
 	return s, nil
+}
+
+// hexToRGBA converts a hex string to a color
+// #RRGGBBAA or #RRGGBB or RRGGBBAA or RRGGBB
+func hexToRGBA(hex string) (col color.RGBA, err error) {
+	// Remove leading '#' if it exists
+	hex = strings.TrimPrefix(hex, "#")
+
+	// Add missing alpha
+	if len(hex) == 6 {
+		hex += "FF"
+	}
+
+	// Parse the hex values for red, green, blue and alpha
+	if len(hex) == 8 {
+		_, err = fmt.Sscanf(hex, "%02x%02x%02x%02x", &col.R, &col.G, &col.B, &col.A)
+		if err != nil {
+			return col, fmt.Errorf("invalid color (%s) : %v", hex, err)
+		}
+	} else {
+		err := errors.New("valid formats : #RRGGBBAA or #RRGGBB or RRGGBBAA or RRGGBB")
+		return col, fmt.Errorf("invalid color (%s) : %v", hex, err)
+	}
+
+	return col, nil
+}
+
+func invert(x int) int {
+	return 7 - x
+}
+
+func abs[T constraints.Float | constraints.Integer](x T) T {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func sgn(dx int) int {
+	switch {
+	case dx < 0:
+		return -1
+	case dx == 0:
+		return 0
+	default:
+		return 1
+	}
 }
